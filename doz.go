f293f7806144e92005c7e38168c5aacb967487ba
TUSF <ragef33@gmail.com>
2017-12-13 18:53:59 -0600

    I forgot to add the variant using turned 2 and 3 for dec and el.

--- doz.go
+++ doz.go
@@ -7,16 +7,20 @@ import (
 
 //	Digits is a slice of string, instead of just a string, because Digits[10] should
 //	return a whole character; not a byte. Exported, so that anyone can modify any
-//	part of the slice for their needs. Be default, `ASCIIDigits` is used.
+//	part of the slice for their needs. Be default, ASCIIDigits is used.
 var Digits = ASCIIDigits
 
 //	An ASCII-friendly set of Dozenal digits. Dec and El are represented using the Latin
 //	letters "X" and "E", to be compatible with most displays.
 var ASCIIDigits = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "X", "E"}
 
-//	The set of Dozenal Digits recommended by The Dozenal Society of America. Dec and El are
+//	The set of Dozenal digits recommended by The Dozenal Society of America. Dec and El are
 //	represented using the Greek letter, Chi (Χ) and the Latin "open E". (Ɛ)
 var AmerDigits = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "\u03A7", "\u0190"}
+
+//	The set of Dozenal digits recommended by The Dozenal Society of Great Britain . Dec and El are
+//	represented using a turned 2 (↊) and 3 (↋). These are the least supported among fonts.
+var BritDigits = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "\u218A", "\u218B"}
 
 //Likely to be initialized a lot, so may as well reuse the same one.
 var zero *big.Int = big.NewInt(0)
